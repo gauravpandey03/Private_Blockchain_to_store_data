c1f187f07726d7e04fa9fbab0d46e177356c3306
gauravpandey03 <gauravpandey10082003@gmail.com>
2024-03-19 15:47:36 +0530

    blockchain created

--- avgtime.js
+++ avgtime.js
@@ -1,26 +1,26 @@
-// const Blockchain = require("./blockchain");
-// const blockchain = new Blockchain();
+const Blockchain = require("./blockchain");
+const blockchain = new Blockchain();
 
-// blockchain.addBlock({ data: "new data" });
-// console.log(blockchain.chain[blockchain.chain.length - 1]);
-// let prevTimestamp, nextTimestamp, nextBlock, timeDiff, averageTime;
+blockchain.addBlock({ data: "new data" });
+console.log(blockchain.chain[blockchain.chain.length - 1]);
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, averageTime;
 
-// const times = [];
+const times = [];
 
-// for (let i = 0; i < 1000; i++) {
-//   prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+for (let i = 0; i < 1000; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
 
-//   blockchain.addBlock({ data: `block ${i}` });
-//   nextBlock = blockchain.chain[blockchain.chain.length - 1];
-//   nextTimestamp = nextBlock.timestamp;
+  blockchain.addBlock({ data: `block ${i}` });
+  nextBlock = blockchain.chain[blockchain.chain.length - 1];
+  nextTimestamp = nextBlock.timestamp;
 
-//   timeDiff = nextTimestamp - prevTimestamp;
+  timeDiff = nextTimestamp - prevTimestamp;
 
-//   times.push(timeDiff);
+  times.push(timeDiff);
 
-//   averageTime = times.reduce((total, num) => total + num) / times.length;
+  averageTime = times.reduce((total, num) => total + num) / times.length;
 
-//   console.log(
-//     `Time to mine block :${timeDiff}ms,Difficulty:${nextBlock.difficulty},Average time:${averageTime}ms`
-//   );
-// }
+  console.log(
+    `Time to mine block :${timeDiff}ms,Difficulty:${nextBlock.difficulty},Average time:${averageTime}ms`
+  );
+}
